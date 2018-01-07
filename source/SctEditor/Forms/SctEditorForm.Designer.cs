c85fedcf2479f97dc03a1fad32f39cd3767f92f5
Andrew Doane <andrew.doane@gmail.com>
2018-01-07 18:27:21 -0400

    Use a real menu instead of buttons

--- source/SctEditor/Forms/SctEditorForm.Designer.cs
+++ source/SctEditor/Forms/SctEditorForm.Designer.cs
@@ -28,47 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.openSctButton = new System.Windows.Forms.Button();
-            this.saveSctButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // openSctButton
+            // menuStrip1
             // 
-            this.openSctButton.Location = new System.Drawing.Point(130, 73);
-            this.openSctButton.Name = "openSctButton";
-            this.openSctButton.Size = new System.Drawing.Size(75, 23);
-            this.openSctButton.TabIndex = 0;
-            this.openSctButton.Text = "Open";
-            this.openSctButton.UseVisualStyleBackColor = true;
-            this.openSctButton.Click += new System.EventHandler(this.openSctButton_Click);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(458, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
             // 
-            // saveSctButton
+            // fileToolStripMenuItem
             // 
-            this.saveSctButton.Location = new System.Drawing.Point(130, 140);
-            this.saveSctButton.Name = "saveSctButton";
-            this.saveSctButton.Size = new System.Drawing.Size(75, 23);
-            this.saveSctButton.TabIndex = 1;
-            this.saveSctButton.Text = "Save";
-            this.saveSctButton.UseVisualStyleBackColor = true;
-            this.saveSctButton.Click += new System.EventHandler(this.saveSctButton_Click);
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save...";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
             // 
             // SctEditorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.saveSctButton);
-            this.Controls.Add(this.openSctButton);
+            this.ClientSize = new System.Drawing.Size(458, 351);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "SctEditorForm";
-            this.Text = "Form1";
+            this.Text = "Skies of Arcadia Script Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button openSctButton;
-        private System.Windows.Forms.Button saveSctButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
--- source/SctEditor/Forms/SctEditorForm.cs
+++ source/SctEditor/Forms/SctEditorForm.cs
@@ -17,50 +17,8 @@ namespace SctEditor
         {
             InitializeComponent();
         }
-
-        private void openSctButton_Click(object sender, EventArgs e)
-        {
-            var openFileDialog = new OpenFileDialog();
-            openFileDialog.CheckFileExists = true;
-            openFileDialog.Filter = "SCT Files (*.sct)|*.sct|All files (*.*)|*.*";            
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                var fileName = openFileDialog.FileName;
-                MemoryStream ms = new MemoryStream(File.ReadAllBytes(fileName));
-                // The first 4 bytes in the file can tell us if this is AKLZ compressed or not.
-                if (ms.ReadString(0, 4) == "AKLZ")
-                {
-                    dataStreamReader = new DataStream(AKLZ.Decompress(ms), Util.Endianness.BigEndian);
-                }
-                else
-                {
-                    dataStreamReader = new DataStream(ms, Util.Endianness.LittleEndian);
-                }
-                currentFile = SctFile.CreateFromStream(dataStreamReader);
-
-                // We first need to decompress the file before getting the SctFile class to parse it.
-                //using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
-                //{
-                //    MemoryStream ms = new MemoryStream();
-                //    // The first 4 bytes in the file can tell us if this is AKLZ compressed or not.
-                //    if (fs.ReadString(0, 4) == "AKLZ")
-                //    {
-                //        ms = AKLZ.Decompress(fs);
-                //    }
-                //    fs.Position = 0;
-                //    var sctFile = SctFile.CreateFromStream(ms);
-
-                //    // Write out the decompressed version for comparison.
-                //    //string decompFileName = Path.GetFileNameWithoutExtension(fileName) + "_decompoo.sct";
-                //    //using (BufferedStream bs = new BufferedStream(new FileStream(decompFileName, FileMode.Create, FileAccess.Write)))
-                //    //{
-                //    //    decompStream.WriteTo(bs);
-                //    //}
-                //}
-            }
-        }
-
-        private void saveSctButton_Click(object sender, EventArgs e)
+       
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
             if (currentFile == null)
             {
@@ -70,6 +28,28 @@ namespace SctEditor
             if (saveDialog.ShowDialog() == DialogResult.OK)
             {
                 currentFile.SaveToFile(saveDialog.FileName, dataStreamReader.Endianness);
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.Filter = "SCT Files (*.sct)|*.sct|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var fileName = openFileDialog.FileName;
+                MemoryStream ms = new MemoryStream(File.ReadAllBytes(fileName));
+                // The first 4 bytes in the file can tell us if this is AKLZ compressed or not.
+                if (ms.ReadString(0, 4) == "AKLZ")
+                {
+                    dataStreamReader = new DataStream(AKLZ.Decompress(ms), Endianness.BigEndian);
+                }
+                else
+                {
+                    dataStreamReader = new DataStream(ms, Endianness.LittleEndian);
+                }
+                currentFile = SctFile.CreateFromStream(dataStreamReader);
             }
         }
     }
